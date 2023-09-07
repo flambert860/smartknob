fb87da4cce57573817540e310037399b8b2101ff
kristian tang <developer@tang.software>
2023-09-07 19:40:45 +0200

    small fix again

--- firmware/src/sound_task.cpp
+++ firmware/src/sound_task.cpp
@@ -1,8 +1,14 @@
 //#if SK_DISPLAY
 #include "sound_task.h"
 #include "semaphore_guard.h"
+#include "sound/XT_I2S_Audio.h"
+#include "sound/WavData.h"
+#include "sound/MusicDefinitions.h"
 
 static const uint8_t LEDC_CHANNEL_LCD_BACKLIGHT = 0;
+
+XT_I2S_Class I2SAudio(I2S_LRC, I2S_BCLK, I2S_DOUT, I2S_NUM_0);
+XT_Wav_Class MySound(WavData16BitStereo);
 
 SoundTask::SoundTask(const uint8_t task_core) : Task{"Sound", 2048, 1, task_core} {
   knob_state_queue_ = xQueueCreate(1, sizeof(PB_SmartKnobState));
